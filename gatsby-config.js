bd1a1a1c0c686f98de2865f7b362fc3d06fb5bf7
ANACA\kirill.bredikhin <kirillb@xello.world>
2020-06-04 21:41:31 -0400

    replaced Google Analytics plugin with gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,10 +32,15 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-plugin-theme-ui`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-168672345-1",
-      },
+        // your google analytics tracking id
+        trackingId: `UA-168672345-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      }
     }
   ],
 }
